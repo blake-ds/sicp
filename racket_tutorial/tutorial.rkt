00cc39968e9868068b3b2b7249ebba88b1242063
Blazej <blazej.marciniak@deepsense.ai>
2022-03-31 10:17:16 +0200

    Update Racket Tutorial

--- racket_tutorial/tutorial.rkt
+++ racket_tutorial/tutorial.rkt
@@ -34,3 +34,38 @@
 (series circle)
 (series square)
 (series (lambda (size) (checkerboard (square size))))
+
+(define (rgb-series mk)
+  (vc-append
+   (series (lambda (sz) (colorize (mk sz) "red")))
+   (series (lambda (sz) (colorize (mk sz) "green")))
+   (series (lambda (sz) (colorize (mk sz) "blue")))))
+
+(rgb-series circle)
+(rgb-series square)
+
+(define (rgb-maker mk)
+  (lambda (sz)
+    (vc-append (colorize (mk sz) "red")
+               (colorize (mk sz) "green")
+               (colorize (mk sz) "blue"))))
+
+
+
+(series (rgb-maker circle))
+(series (rgb-maker square))
+
+
+
+(list "red" "green" "blue")
+(list (circle 10) (square 10))
+
+; Returns a list of shapes
+(define (rainbow p)
+  (map (lambda (color)
+         (colorize p color))
+       (list "red" "orange" "yellow" "green" "blue" "purple")))
+
+(rainbow (square 10))
+(apply vc-append (rainbow (square 5)))
+(apply vc-append (rainbow (filled-rectangle 200 5)))
