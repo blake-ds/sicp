9437ebccac51d0bdb8ee142f7530b88fc990d784
Blazej <blazej.marciniak@deepsense.ai>
2022-03-31 20:40:51 +0200

    Add fibonacci

--- 1. Building Abstractions with Procedures & Functions/1_009.rkt
+++ 1. Building Abstractions with Procedures & Functions/1_009.rkt
@@ -1,10 +1,12 @@
 #lang sicp
+; the first procedure is a recursive process
 (define (+ a b)
   (if (= a 0)
       b
       (inc (+ (dec a) b))))
 
-(define (+ a b)
+; the second procedure is iterative
+(define (+other a b)
   (if (= a 0)
       b
       (+ (dec a) (inc b))))
