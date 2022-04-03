cb899f558dd69982715449c1511535456ff08ad8
Blazej <blazej.marciniak@deepsense.ai>
2022-04-03 14:59:37 +0200

    Add 1 21:

---
+++ 1. Building Abstractions with Procedures & Functions/1_021.rkt
@@ -0,0 +1,18 @@
+#lang racket/base
+
+(define (square n) (* n n))
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 199)
+(smallest-divisor 100)
+(smallest-divisor 1999)
+(smallest-divisor 1999)
